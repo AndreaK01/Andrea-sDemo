286f0be2d2ebf7bd06d58b00fb9225d9f21ec781
AndreaK01 <71346523+AndreaK01@users.noreply.github.com>
2020-09-21 19:41:24 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 var boxEle = document.body.querySelector(".box");
 
-var userInput = prompt("What is your name? ");
+var userInput = prompt("Enter a number ");
+var userInput2 = prompt("Enter another number ");
 
-var numberOne = 8;
-var numberTwo = 2;
+boxEle.innerHTML = userInput+userInput2;
 
-boxEle.innerHTML = userInput;
-
-boxEle.innerHTML = numberOne+numberTwo;
+boxEle.innerHTML = userInput, userInput2;
